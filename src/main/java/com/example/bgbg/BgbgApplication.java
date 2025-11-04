f71bf12f62171d56ab612640353a3b0d3b54f7ef
egaeuni <gaeun3619@gmail.com>
2025-11-04 21:19:47 +0900

    :tada: Init: 프로젝트 세팅

---
+++ src/main/java/com/example/bgbg/BgbgApplication.java
@@ -0,0 +1,13 @@
+package com.example.bgbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgbgApplication.class, args);
+    }
+
+}
